68a24b5be847291cfd031a7a07af167d096c1cf5
linndonn <linndonn@usf.edu>
2025-01-26 04:31:26 -0500

    Task 1 - Employee Information.

---
+++ cc_1.js
@@ -0,0 +1,18 @@
+//Task 1: Variables
+
+let employeeName = "Jemella Bascus";
+//Declare a variable employeeName using let and assign it the employee's name as a string.
+
+const employeeID = 1268;
+//Declare a variable employeeID using const and assign it the employee's ID as a number.
+
+var isActive = true;
+//Declare a variable isActive using var and assign it a boolean value true to indicate the employee is currently active.
+
+console.log(typeof employeeName);
+console.log(typeof employeeID);
+console.log(typeof isActive);
+//Log each variable's value and its type to the console using console.log() and typeof.
+
+
+//Task 2: Primitive Data Type
